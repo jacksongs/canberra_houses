d03a7a709222cad51d84026a269a405fdbf69527
Jackson Gothe-Snape <jacksongs@gmail.com>
2015-03-09 14:13:57 +1100

    Removed slice so that it runs across all suburbs

--- scraper.py
+++ scraper.py
@@ -66,7 +66,7 @@ except:
 	pass
 
 
-for link in scraperwiki.sql.select("* from suburbs")[8:11]:
+for link in scraperwiki.sql.select("* from suburbs"):
 
 	houses = [] # this is to hold all the data on the houses in each suburb for saving to the database
 	
