02f47a454a422d858404f283fd6613fd18d87943
Jackson Gothe-Snape <jacksongs@gmail.com>
2015-03-30 16:05:57 +1100

    Removed slice

--- scraper.py
+++ scraper.py
@@ -198,7 +198,7 @@ except:
 	pass
 
 
-for link in scraperwiki.sql.select("* from suburbs")[16:19]:
+for link in scraperwiki.sql.select("* from suburbs"):
 
 	houses = [] # this is to hold all the data on the houses in each suburb for saving to the database
 	
