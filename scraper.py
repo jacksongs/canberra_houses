5b4ed00aaf4d8b66f63262fb1e7b59258c5c1ec9
Jackson Gothe-Snape <jacksongs@gmail.com>
2015-03-30 16:04:43 +1100

    Tightened up scraper, added regions

--- scraper.py
+++ scraper.py
@@ -13,12 +13,162 @@ import dateutil.parser
 suburbs = requests.get("http://www.allhomes.com.au/ah/act/sale-residential/canberra/1039110")
 
 soup = BeautifulSoup(suburbs.content)
-dds = soup.find_all("dd")
+column = soup.find("div",class_="home_page_column")
+dds = column.find_all("dd")
+
+region = {}
+region["Aranda"] = "Belconnen"
+region["Belconnen"] = "Belconnen"
+region["Bruce"] = "Belconnen"
+region["Charnwood"] = "Belconnen"
+region["Cook"] = "Belconnen"
+region["Dunlop"] = "Belconnen"
+region["Emu Ridge"] = "Belconnen"
+region["Evatt"] = "Belconnen"
+region["Florey"] = "Belconnen"
+region["Flynn"] = "Belconnen"
+region["Fraser"] = "Belconnen"
+region["Giralang"] = "Belconnen"
+region["Hawker"] = "Belconnen"
+region["Higgins"] = "Belconnen"
+region["Holt"] = "Belconnen"
+region["Kaleen"] = "Belconnen"
+region["Latham"] = "Belconnen"
+region["Lawson"] = "Belconnen"
+region["Macgregor"] = "Belconnen"
+region["Macquarie"] = "Belconnen"
+region["Mckellar"] = "Belconnen"
+region["Melba"] = "Belconnen"
+region["Page"] = "Belconnen"
+region["Scullin"] = "Belconnen"
+region["South Bruce"] = "Belconnen"
+region["Spence"] = "Belconnen"
+region["Weetangera"] = "Belconnen"
+region["Uriarra Village"]="Coree"
+region["Burra"]="Greater Queanbeyan"
+region["Crestwood"]="Greater Queanbeyan"
+region["Fernleigh Park"]="Greater Queanbeyan"
+region["Googong"]="Greater Queanbeyan"
+region["Greenleigh Estate"]="Greater Queanbeyan"
+region["Jerrabomberra"]="Greater Queanbeyan"
+region["Karabar"]="Greater Queanbeyan"
+region["Kingsway"]="Greater Queanbeyan"
+region["Little Burra"]="Greater Queanbeyan"
+region["Mount Campbell"]="Greater Queanbeyan"
+region["Queanbeyan"]="Greater Queanbeyan"
+region["Queanbeyan East"]="Greater Queanbeyan"
+region["Queanbeyan West"]="Greater Queanbeyan"
+region["Royalla"]="Greater Queanbeyan"
+region["Royalla Estate"]="Greater Queanbeyan"
+region["The Ridgeway"]="Greater Queanbeyan"
+region["Tralee"]="Greater Queanbeyan"
+region["Weetalabah Estate"]="Greater Queanbeyan"
+region["Amaroo"]="Gungahlin"
+region["Bonner"]="Gungahlin"
+region["Broadview"]="Gungahlin"
+region["Casey"]="Gungahlin"
+region["Crace"]="Gungahlin"
+region["Forde"]="Gungahlin"
+region["Franklin"]="Gungahlin"
+region["Gungahlin"]="Gungahlin"
+region["Hall"]="Gungahlin"
+region["Harcourt Hill"]="Gungahlin"
+region["Harrison"]="Gungahlin"
+region["Jacka"]="Gungahlin"
+region["Mitchell"]="Gungahlin"
+region["Moncrieff"]="Gungahlin"
+region["Ngunnawal"]="Gungahlin"
+region["Nicholls"]="Gungahlin"
+region["Palmerston"]="Gungahlin"
+region["Springbank Rise"]="Gungahlin"
+region["Yerrabi"]="Gungahlin"
+region["Coombs"]="Molonglo Valley"
+region["Wright"]="Molonglo Valley"
+region["Acton"]="North Canberra"
+region["Ainslie"]="North Canberra"
+region["Anu"]="North Canberra"
+region["Braddon"]="North Canberra"
+region["Campbell"]="North Canberra"
+region["Canberra"]="North Canberra"
+region["City"]="North Canberra"
+region["Dickson"]="North Canberra"
+region["Downer"]="North Canberra"
+region["Hackett"]="North Canberra"
+region["Lyneham"]="North Canberra"
+region["Majura"]="North Canberra"
+region["NewActon"]="North Canberra"
+region["North Lyneham"]="North Canberra"
+region["O'Connor"]="North Canberra"
+region["Reid"]="North Canberra"
+region["Russell"]="North Canberra"
+region["Turner"]="North Canberra"
+region["Watson"]="North Canberra"
+region["Barton"]="South Canberra"
+region["Deakin"]="South Canberra"
+region["Forrest"]="South Canberra"
+region["Fyshwick"]="South Canberra"
+region["Griffith"]="South Canberra"
+region["Kingston"]="South Canberra"
+region["Manuka"]="South Canberra"
+region["Narrabundah"]="South Canberra"
+region["Oaks Estate"]="South Canberra"
+region["Pialligo"]="South Canberra"
+region["Red Hill"]="South Canberra"
+region["Symonston"]="South Canberra"
+region["Yarralumla"]="South Canberra"
+region["Banks"]="Tuggeranong"
+region["Bonython"]="Tuggeranong"
+region["Calwell"]="Tuggeranong"
+region["Chisholm"]="Tuggeranong"
+region["Conder"]="Tuggeranong"
+region["Conder Ridge"]="Tuggeranong"
+region["Fadden"]="Tuggeranong"
+region["Fadden Hills"]="Tuggeranong"
+region["Gilmore"]="Tuggeranong"
+region["Gleneagles"]="Tuggeranong"
+region["Gordon"]="Tuggeranong"
+region["Gowrie"]="Tuggeranong"
+region["Greenway"]="Tuggeranong"
+region["Hume"]="Tuggeranong"
+region["Isabella Plains"]="Tuggeranong"
+region["Kambah"]="Tuggeranong"
+region["Macarthur"]="Tuggeranong"
+region["Monash"]="Tuggeranong"
+region["Monash Ridge"]="Tuggeranong"
+region["Oxley"]="Tuggeranong"
+region["Richardson"]="Tuggeranong"
+region["Tharwa"]="Tuggeranong"
+region["Theodore"]="Tuggeranong"
+region["Tuggeranong"]="Tuggeranong"
+region["Wanniassa"]="Tuggeranong"
+region["Chapman"]="Weston Creek"
+region["Duffy"]="Weston Creek"
+region["Fisher"]="Weston Creek"
+region["Holder"]="Weston Creek"
+region["McCubbin Rise"]="Weston Creek"
+region["Rivett"]="Weston Creek"
+region["Stirling"]="Weston Creek"
+region["Waramanga"]="Weston Creek"
+region["Weston"]="Weston Creek"
+region["Chifley"]="Woden Valley"
+region["Curtin"]="Woden Valley"
+region["Farrer"]="Woden Valley"
+region["Garran"]="Woden Valley"
+region["Hughes"]="Woden Valley"
+region["Isaacs"]="Woden Valley"
+region["Lyons"]="Woden Valley"
+region["Mawson"]="Woden Valley"
+region["O'Malley"]="Woden Valley"
+region["Pearce"]="Woden Valley"
+region["Phillip"]="Woden Valley"
+region["Swinger Hill"]="Woden Valley"
+region["Torrens"]="Woden Valley"
+region["Woden"]="Woden Valley"
 
 subs = []
 for dd in dds:
-	subs.append({"Name":dd.a.text,"Link":dd.a.get('href')})
-	
+	subs.append({"Name":dd.a.text,"Link":dd.a.get('href'),"Region":region[dd.a.text]})
+
 scraperwiki.sqlite.save(unique_keys=["Link"],data=subs,table_name='suburbs')
 
 try:
@@ -48,7 +198,7 @@ except:
 	pass
 
 
-for link in scraperwiki.sql.select("* from suburbs"):
+for link in scraperwiki.sql.select("* from suburbs")[16:19]:
 
 	houses = [] # this is to hold all the data on the houses in each suburb for saving to the database
 	
@@ -110,6 +260,7 @@ for link in scraperwiki.sql.select("* from suburbs"):
 			try:
 				house["Address 1"]=tr.td.next_sibling.next_sibling.div.div.a.text.split(",")[0].strip()
 				house["Suburb"]=link["Name"]
+				house["Region"]=link["Region"]
 				house["Link"]=tr.td.next_sibling.next_sibling.div.div.a.get("href")
 				house["Added"] = datetime.datetime.now()
 				house["Active"] = True
@@ -166,6 +317,7 @@ for link in scraperwiki.sql.select("* from suburbs"):
 				try:
 					listing = {}
 					listing["Suburb"] = link["Name"]
+					listing["Region"] = link["Region"]
 					listing["Link"]=tr.td.next_sibling.next_sibling.div.div.a.get("href")
 					listing["Sold"] = tr.find("span",class_="badge-sold")!=None
 					listing["New"] = tr.find("span",class_="badge-new")!=None
